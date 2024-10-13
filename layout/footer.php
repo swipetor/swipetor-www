a735206f3d0d71aeb196ba77cdf91dfcff2de137
AtaS <atasasmaz@gmail.com>
2024-10-13 22:19:06 +0100

    Add tracking code

--- layout/footer.php
+++ layout/footer.php
@@ -6,7 +6,23 @@
     setBodyWrapperMarginTop();
 </script>
 
-<!-- TODO: Add your analytics -->
+<!-- Matomo -->
+<script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['setRequestMethod', 'POST']);
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+
+    (function () {
+        var u = "//mtm.swipetor.com/";
+        _paq.push(['setTrackerUrl', u + 'mtm']);
+        _paq.push(['setSiteId', '7']);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.async = true; g.src = u + 'mtm.js'; s.parentNode.insertBefore(g, s);
+    })();
+</script>
+<!-- End Matomo Code -->
 
 </body>
 
