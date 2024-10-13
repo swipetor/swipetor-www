8d10a0b57e438e4670b3fb79ad7a6e4ec6a0fefc
AtaS <atasasmaz@gmail.com>
2024-10-13 14:40:27 +0100

    Index styling

--- index.php
+++ index.php
@@ -26,15 +26,16 @@ standing out from the influx of mediocre and AI-generated content on social medi
 
 <div class="homeLinksDiv">
     <?php if (isset($ssg->config->github_url) && strlen($ssg->config->github_url) > 0) { ?>
-        <a href="https://demo.swipetor.com" target="_blank">demo.swipetor.com</a> (nicer on phone)
-        <span class="sep">
-            |
-        </span>
-        <a href="<?= $ssg->config->github_url ?>" target="_blank" rel="nofollow noreferrer" title="on GitHub"
-            class="githubLink">
-            <img src="/assets/images/github-icon.svg" alt="GitHub icon" />
-            <?= str_replace('https://', '', $ssg->config->github_url) ?>
-        </a>
+        <div>
+            <a href="https://demo.swipetor.com" target="_blank">demo.swipetor.com</a> (nicer on phone)
+        </div>
+        <div>
+            <a href="<?= $ssg->config->github_url ?>" target="_blank" rel="nofollow noreferrer" title="on GitHub"
+                class="githubLink">
+                <img src="/assets/images/github-icon.svg" alt="GitHub icon" />
+                <?= str_replace('https://', '', $ssg->config->github_url) ?>
+            </a>
+        </div>
     <?php } ?>
 </div>
 
@@ -56,8 +57,7 @@ standing out from the influx of mediocre and AI-generated content on social medi
     <li>Possibility of cloud hosting with no setup - contact and check</li>
 </ul>
 
-<br />
-<p>
+<p style="margin-top: 20px;">
     <strong>Contact</strong> —
     <span class="email"></span>
 </p>
